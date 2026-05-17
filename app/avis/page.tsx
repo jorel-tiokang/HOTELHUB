47ffaafe151f26d09b11e1a5f1730a98e3119bad
jorel-tiokang <jotioks@gmail.com>
2026-05-17 13:30:57 +0100

    Update page.tsx

--- app/avis/page.tsx
+++ app/avis/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const AvisPage = () => {
+  return <div>AvisPage</div>;
 };
 
-export default page;
+export default AvisPage;
